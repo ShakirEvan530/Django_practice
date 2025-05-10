0c114936fa53831a02f43716f3e91b4c937171c1
ShakirEvan530 <ausshakirahmed@gmail.com>
2025-05-10 09:50:06 +0600

    dynamic rendeer

--- my_project2/my_app2/urls.py
+++ my_project2/my_app2/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from my_app2.views import learning_django
-from my_app2.views import learning_api
+from my_app2.views import learning_django,learning_api
+# from my_app2.views import learning_api
 urlpatterns = [
     path('dj/',learning_django),
     path('lapi/',learning_api),
--- my_project2/my_app2/views.py
+++ my_project2/my_app2/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
 def learning_django(req):
-    return render(req,'my_app2/django.html')
+    coursename = {'cname':'Django 3.0'}
+    return render(req,'my_app2/django.html',coursename)
 
 
 def learning_api(req):
-    return render(req,'my_app2/fastapi.html')
+    course = {'cname':'FastApi', 'version':'3.25'}
+    return render(req,'my_app2/fastapi.html',course)
